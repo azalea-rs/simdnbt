f89dc8e2bc507d9b84878586d07cb77ab338497b
mat <git@matdoes.dev>
2024-01-11 14:51:20 -0600

    slightly improve doc

--- simdnbt/src/lib.rs
+++ simdnbt/src/lib.rs
@@ -1,11 +1,18 @@
-//! an unnecessarily fast nbt decoder.
-//!
-//! afaik, this is currently the fastest nbt decoder in existence.
+//! simdnbt is a very fast nbt serializer and deserializer.
+//! 
+//! It comes in two variants:
+//! - [`borrow`](borrow), which can only read and requires you to keep a reference to the original buffer.
+//! - [`owned`](owned), which can read and write and owns the data.
+//! 
+//! `borrow` will always be faster, but can't be used for every use case.
+//! 
+//! ## Example
 //!
 //! ```
 //! use simdnbt::borrow::Nbt;
 //! use std::io::Cursor;
 //!
+//! // Read
 //! let nbt = Nbt::read(&mut Cursor::new(include_bytes!("../tests/hello_world.nbt"))).unwrap().unwrap();
 //! assert_eq!(nbt.name().to_str(), "hello world");
 //! assert_eq!(nbt.string("name").unwrap().to_str(), "Bananrama");
