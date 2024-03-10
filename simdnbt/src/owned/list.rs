33b8f0db99493f3920914f280285030fdf51bbed
mat <git@matdoes.dev>
2024-03-09 19:45:29 -0600

    fix NbtList::Double write code to be consistent with the others

--- simdnbt/src/owned/list.rs
+++ simdnbt/src/owned/list.rs
@@ -147,8 +147,7 @@ impl NbtList {
                 write_with_u32_length(data, 4, &slice_into_u8_big_endian(floats));
             }
             NbtList::Double(doubles) => {
-                let bytes = slice_into_u8_big_endian(doubles);
-                write_with_u32_length(data, 8, &bytes);
+                write_with_u32_length(data, 8, &slice_into_u8_big_endian(doubles));
             }
             NbtList::ByteArray(byte_arrays) => {
                 write_u32(data, byte_arrays.len() as u32);
