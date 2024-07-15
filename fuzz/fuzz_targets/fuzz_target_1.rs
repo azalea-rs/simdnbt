4fa1f11f760992acbcddd8bc927773a7d69e674c
mat <git@matdoes.dev>
2024-07-15 03:26:18 +0000

    fix clippy warning in fuzz code

--- fuzz/fuzz_targets/fuzz_target_1.rs
+++ fuzz/fuzz_targets/fuzz_target_1.rs
@@ -3,9 +3,9 @@
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
-    if let Ok(r) = simdnbt::borrow::read(&mut std::io::Cursor::new(data)) {
-        if let simdnbt::borrow::Nbt::Some(r) = r {
-            r.as_compound().to_owned();
-        }
+    if let Ok(simdnbt::borrow::Nbt::Some(r)) =
+        simdnbt::borrow::read(&mut std::io::Cursor::new(data))
+    {
+        r.as_compound().to_owned();
     }
 });
