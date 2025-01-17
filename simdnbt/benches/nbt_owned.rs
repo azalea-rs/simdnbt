01004062e1c60e47f71c0d1ec6043b5596b90918
mat <git@matdoes.dev>
2025-01-17 06:45:26 +0000

    update dependencies and comment out a broken benchmark

--- simdnbt/benches/nbt_owned.rs
+++ simdnbt/benches/nbt_owned.rs
@@ -35,14 +35,14 @@ fn bench_file(filename: &str, c: &mut Criterion) {
     let nbt = simdnbt::owned::read(&mut decoded_src_stream)
         .unwrap()
         .unwrap();
-    group.bench_function("Get", |b| {
-        b.iter(|| {
-            let level = nbt.compound("abilities").unwrap();
-            for (k, _) in level.iter() {
-                black_box(level.get(black_box(&k.to_str())));
-            }
-        })
-    });
+    // group.bench_function("Get", |b| {
+    //     b.iter(|| {
+    //         let level = nbt.compound("abilities").unwrap();
+    //         for (k, _) in level.iter() {
+    //             black_box(level.get(black_box(&k.to_str())));
+    //         }
+    //     })
+    // });
     group.bench_function("Encode", |b| {
         b.iter(|| {
             let mut out = Vec::new();
